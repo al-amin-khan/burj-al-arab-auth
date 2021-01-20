360e6d7ce8a57e1bdea4ce99b15a5040a11f4595
al-amin-khan <alaminkhan2010@gmail.com>
2021-01-21 00:29:19 +0600

    initialize app

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+
+
+const Login = () => {
+    return (
+        <div>
+            <h1>This is Login</h1>
+        </div>
+    );
+};
+
+export default Login;
